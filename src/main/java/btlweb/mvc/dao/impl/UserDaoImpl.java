63997d620ccbafb5ff9d9612a154cd2f651ede26
AshHogan <111612126+AshHogan@users.noreply.github.com>
2023-04-20 00:30:54 +0700

    Update UserDaoImpl.java

--- src/main/java/btlweb/mvc/dao/impl/UserDaoImpl.java
+++ src/main/java/btlweb/mvc/dao/impl/UserDaoImpl.java
@@ -1,52 +1,179 @@
 package btlweb.mvc.dao.impl;
 
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
-import btlweb.mvc.dao.UserDao;
+import btlweb.mvc.dbconnect.MySQLConnect;
+import btlweb.mvc.model.Role;
 import btlweb.mvc.model.User;
+import btlweb.mvc.dao.UserDao;
 
 public class UserDaoImpl implements UserDao{
-
+	private Connection conn = null;
+	private PreparedStatement ps = null;
+	private ResultSet rs = null;
+	   
 	@Override
 	public User findUserByEmail(String email) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		User user = null;
+		   try {
+		      conn = MySQLConnect.getConnection();
+		      String sql = "SELECT * FROM user WHERE id = ?";
+		      ps = conn.prepareStatement(sql);
+		      ps.setString(1, email);
+		      rs = ps.executeQuery();
+		      if (rs.next()) {
+		         user = new User();
+		         user.setId(rs.getInt("id"));
+		         user.setEmail(rs.getString("email"));
+		         user.setFirstName(rs.getString("firstName"));
+		         user.setLastName(rs.getString("lastName"));
+		         user.setPassword(rs.getString("password"));
+		      }
+		   } catch (SQLException e) {
+		      e.printStackTrace();
+		   } 
+		   return user;
+		}
+		
 
 	@Override
 	public User findUserById(int id) {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		   
+		   User user = null;
+		   try {
+		      conn = MySQLConnect.getConnection();
+		      String sql = "SELECT * FROM user WHERE id = ?";
+		      ps = conn.prepareStatement(sql);
+		      ps.setInt(1, id);
+		      rs = ps.executeQuery();
+		      if (rs.next()) {
+		         user = new User();
+		         user.setId(rs.getInt("id"));
+		         user.setFirstName(rs.getString("firstName"));
+		         user.setLastName(rs.getString("lastName"));
+		         user.setPassword(rs.getString("password"));
+		      }
+		   } catch (SQLException e) {
+		      e.printStackTrace();
+		   } 
+		   return user;
+		}
 
 	@Override
 	public int insertUser(User user) {
 		// TODO Auto-generated method stub
-		return 0;
+		int rows = 0;
+		try {
+		      conn = MySQLConnect.getConnection();
+		      String sql = "INSERT INTO user (firstName, lastName, email, password, phoneNumber, city, district, streetAddress, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+		      ps = conn.prepareStatement(sql);
+		      ps.setString(1, user.getFirstName());
+		      ps.setString(2, user.getLastName());
+		      ps.setString(3, user.getEmail());
+		      ps.setString(4, user.getPassword());
+		      ps.setString(5, user.getPhoneNumber());
+		      ps.setString(6, user.getCity());
+		      ps.setString(7, user.getDistrict());
+		      ps.setString(8, user.getStreetAddress());
+		      rows = ps.executeUpdate();
+		   } catch (SQLException e) {
+		      e.printStackTrace();
+		   }
+		return rows;
 	}
 
 	@Override
 	public int delete(int id) {
 		// TODO Auto-generated method stub
-		return 0;
+		int rows = 0;
+	    try {
+	        conn = MySQLConnect.getConnection();
+	        String sql = "DELETE FROM user WHERE id = ?";
+	        ps = conn.prepareStatement(sql);
+	        ps.setInt(1, id);
+	        rows = ps.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return rows;
 	}
-
 	@Override
 	public int update(User user) {
 		// TODO Auto-generated method stub
-		return 0;
+		int rows = 0;
+	    try {
+	        conn = MySQLConnect.getConnection();
+	        String sql = "UPDATE user SET firstName = ?, lastName = ?, email = ?, password = ?, phoneNumber = ?, city = ?, district = ?, streetAddress = ? WHERE id = ?";
+	        ps = conn.prepareStatement(sql);
+	        ps.setString(1, user.getFirstName());
+		    ps.setString(2, user.getLastName());
+		    ps.setString(3, user.getEmail());
+		    ps.setString(4, user.getPassword());
+		    ps.setString(5, user.getPhoneNumber());
+	        ps.setString(6, user.getCity());
+		    ps.setString(7, user.getDistrict());
+		    ps.setString(8, user.getStreetAddress());
+	        ps.setInt(9, user.getId());
+	        rows = ps.executeUpdate();
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return rows;
 	}
-
 	@Override
 	public List<User> getUserInPage(int pageId) {
 		// TODO Auto-generated method stub
-		return null;
+		List<User> userList = new ArrayList<>();
+	    try {
+	        conn = MySQLConnect.getConnection();
+	        int pageSize = 8; // Số lượng user trong một trang
+	        int offset = (pageId - 1) * pageSize; // Vị trí bắt đầu lấy user
+	        String sql = "SELECT * FROM users LIMIT ? OFFSET ?";
+	        ps = conn.prepareStatement(sql);
+	        ps.setInt(1, pageSize);
+	        ps.setInt(2, offset);
+	        rs = ps.executeQuery();
+	        while (rs.next()) {
+	            User user = new User();
+	            user.setId(rs.getInt("id"));
+	            user.setFirstName(rs.getString("firstName"));
+	            user.setLastName(rs.getString("lastName"));
+	            user.setEmail(rs.getString("email"));
+	            user.setPassword(rs.getString("password"));
+	            user.setCity(rs.getString("city"));
+	            user.setDistrict(rs.getString("district"));
+	            user.setStreetAddress(rs.getString("streetAddress"));
+	            userList.add(user);
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return userList;
 	}
 
 	@Override
 	public int getNumOfUser() {
 		// TODO Auto-generated method stub
-		return 0;
+		int count = 0;
+	    try {
+	        conn = MySQLConnect.getConnection();
+	        String sql = "SELECT COUNT(*) FROM user";
+	        ps = conn.prepareStatement(sql);
+	        rs = ps.executeQuery();
+	        if (rs.next()) {
+	            count = rs.getInt(1);
+	        }
+	    } catch (SQLException e) {
+	        e.printStackTrace();
+	    }
+	    return count;
 	}
-
 }
+
